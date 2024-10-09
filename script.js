d8e210382cf495885161b007f90a780c072f35f1
prem-singh333 <premkumarsingh7241@gmail.com>
2024-10-09 23:27:43 +0530

    Add some effects

--- script.js
+++ script.js
@@ -1,54 +1,96 @@
-let menu =  document.querySelector(".menu");
-let cross = document.querySelector(".cross");
-let sidebar = document.querySelector(".side-bar");
+/**===================== Header-Section=======================**/
 
 let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector(".menu")
+let sidebar = document.querySelector(".sidebar");
+let cross = document.querySelector(".cross");
 
-let text = document.querySelector(".sec-text");
 
 
 cross.style.display = "none";
-menu.style.display = "inline";
 
 hamburger.addEventListener("click",() => {
-    sidebar.classList.toggle("sidebar-go");
+    console.log("menu was clicked");
+    sidebar.classList.toggle("sidebargo");
 
-        if( sidebar.classList.contains("sidebar-go")){
-            setTimeout(() => {
-                menu.style.display = "inline";
-            },200);
+    if(sidebar.classList.contains("sidebargo")){
+
+       
+        setTimeout(() => {
+            menu.style.display = "inline";
+        },200);
+
+        setTimeout(() => {
             cross.style.display = "none";
-        }else{
-            setTimeout(() => {
-                menu.style.display = "none";
-            },200);
+        },190);
 
-            setTimeout(() => {
-                cross.style.display = "inline";
-            },350);
-        }
+    }else{
+
+        setTimeout(() => {
+            menu.style.display = "none";
+        },295);
+
+        setTimeout(() => {
+            cross.style.display = "inline";
+        },300);
+    }
 
 });
 
-let textLoad = () => {
 
-    setTimeout(() => {
-        text.textContent = "Frontend Developer";
-    },0);
 
-    setTimeout(() => {
-        text.textContent = "UI/UX Designer";
-    },5000);
 
-    setTimeout(() => {
-        text.textContent = "Freelancer";
-    },10000);
+// let menu =  document.querySelector(".menu");
+// let cross = document.querySelector(".cross");
+// let sidebar = document.querySelector(".side-bar");
 
-    setTimeout(() => {
-        text.textContent = "Coder";
-    },15000);
-}
+// let hamburger = document.querySelector(".hamburger");
 
-textLoad();
+// let text = document.querySelector(".sec-text");
 
-setInterval(textLoad, 20000);
+
+// cross.style.display = "none";
+// menu.style.display = "inline";
+
+// hamburger.addEventListener("click",() => {
+//     sidebar.classList.toggle("sidebar-go");
+
+//         if( sidebar.classList.contains("sidebar-go")){
+//             setTimeout(() => {
+//                 menu.style.display = "inline";
+//             },200);
+//             cross.style.display = "none";
+//         }else{
+//             setTimeout(() => {
+//                 menu.style.display = "none";
+//             },200);
+
+//             setTimeout(() => {
+//                 cross.style.display = "inline";
+//             },350);
+//         }
+
+// });
+
+// let textLoad = () => {
+
+//     setTimeout(() => {
+//         text.textContent = "Frontend Developer";
+//     },0);
+
+//     setTimeout(() => {
+//         text.textContent = "UI/UX Designer";
+//     },5000);
+
+//     setTimeout(() => {
+//         text.textContent = "Freelancer";
+//     },10000);
+
+//     setTimeout(() => {
+//         text.textContent = "Coder";
+//     },15000);
+// }
+
+// textLoad();
+
+// setInterval(textLoad, 20000);
